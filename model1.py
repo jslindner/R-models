aaf1da0e41cda2e13f28ac9debe2f364424d42a0
thayertoes <33706638+thayertoes@users.noreply.github.com>
2018-04-23 14:49:37 -0700

    Update model1.py

--- model1.py
+++ model1.py
@@ -2,46 +2,6 @@ import random
 import tkinter
 random.seed()
 
-def plot_time(to_plot):
-    spp1xs = to_plot[0]
-    spp1ys = to_plot[1]
-    spp2ys = to_plot[2]
-    res1ys = to_plot[3]
-    res2ys = to_plot[4]
-    gens = to_plot[5]
-    space = round(600/gens)
-    root = tkinter.Tk()
-    c = tkinter.Canvas(root, width=700, height=600, bg='white')
-    c.grid()
-    # Create the x-axis.
-    c.create_line(50,550,650,550, width=3)
-    for i in range(gens+1):
-        x = 50 + (i * space)
-        c.create_text(x,555,anchor='n', text='%s'% i)
-    # Create the y-axis.
-    c.create_line(50,550,50,50, width=3)
-    for i in range(11):
-        y = 550 - (i * 50)
-        c.create_text(45,y, anchor='e', text='%s'% (200*i))
-    c.create_text(400,200,text='spp1 red\nspp2 green\nres1 blue\nres2 yellow')
-    # Plot the points.
-    for i in range(gens):
-        spp1x, spp1y = spp1xs[i], spp1ys[i]
-        spp1xpixel = int(50 + space * spp1x)
-        spp1ypixel = int(550 - spp1y/4)
-        c.create_line(spp1xpixel, spp1ypixel, int(50 + space * spp1xs[i-1]), int(550 - spp1ys[i-1]/4), fill = 'red', width = 2)
-        c.create_oval(spp1xpixel-3,spp1ypixel-3,spp1xpixel+3,spp1ypixel+3, width=1, fill='red')
-        spp2ypixel = int(550 - spp2ys[i]/4)
-        c.create_line(spp1xpixel, spp2ypixel, int(50 + space * spp1xs[i-1]), int(550 - spp2ys[i-1]/4), fill = 'green', width = 2)
-        c.create_oval(spp1xpixel-3,spp2ypixel-3,spp1xpixel+3,spp2ypixel+3, width=1, fill='green')
-        res1ypixel = int(550 - res1ys[i]/4)
-        c.create_line(spp1xpixel, res1ypixel, int(50 + space * spp1xs[i-1]), int(550 - res1ys[i-1]/4), fill = 'blue', width = 2)
-        c.create_rectangle(spp1xpixel-3,res1ypixel-3,spp1xpixel+3,res1ypixel+3, width=1, fill='blue')
-        res2ypixel = int(550 - res2ys[i]/4)
-        c.create_line(spp1xpixel, res2ypixel, int(50 + space * spp1xs[i-1]), int(550 - res2ys[i-1]/4), fill = 'yellow', width = 2)
-        c.create_rectangle(spp1xpixel-3,res2ypixel-3,spp1xpixel+3,res2ypixel+3, width=1, fill='yellow')
-    #root.mainloop()
-
 class World:
     def __init__(self, spp1, spp2, res1, res2):
         self.spp1 = spp1
@@ -121,8 +81,48 @@ def test_nums(spp1s, spp1r, spp1m, spp1k, spp1res1lim, spp1res2lim, spp2s, spp2r
         i += 1
     to_plot = [spp1xs,spp1ys,spp2ys,res1ys,res2ys,reps]
     return to_plot
+
 def test_time(to_plot):
     plot_time(to_plot)
+def plot_time(to_plot):
+    spp1xs = to_plot[0]
+    spp1ys = to_plot[1]
+    spp2ys = to_plot[2]
+    res1ys = to_plot[3]
+    res2ys = to_plot[4]
+    gens = to_plot[5]
+    space = round(600/gens)
+    root = tkinter.Tk()
+    c = tkinter.Canvas(root, width=700, height=600, bg='white')
+    c.grid()
+    # Create the x-axis.
+    c.create_line(50,550,650,550, width=3)
+    for i in range(gens+1):
+        x = 50 + (i * space)
+        c.create_text(x,555,anchor='n', text='%s'% i)
+    # Create the y-axis.
+    c.create_line(50,550,50,50, width=3)
+    for i in range(11):
+        y = 550 - (i * 50)
+        c.create_text(45,y, anchor='e', text='%s'% (200*i))
+    c.create_text(400,200,text='spp1 red\nspp2 green\nres1 blue\nres2 yellow')
+    # Plot the points.
+    for i in range(gens):
+        spp1x, spp1y = spp1xs[i], spp1ys[i]
+        spp1xpixel = int(50 + space * spp1x)
+        spp1ypixel = int(550 - spp1y/4)
+        c.create_line(spp1xpixel, spp1ypixel, int(50 + space * spp1xs[i-1]), int(550 - spp1ys[i-1]/4), fill = 'red', width = 2)
+        c.create_oval(spp1xpixel-3,spp1ypixel-3,spp1xpixel+3,spp1ypixel+3, width=1, fill='red')
+        spp2ypixel = int(550 - spp2ys[i]/4)
+        c.create_line(spp1xpixel, spp2ypixel, int(50 + space * spp1xs[i-1]), int(550 - spp2ys[i-1]/4), fill = 'green', width = 2)
+        c.create_oval(spp1xpixel-3,spp2ypixel-3,spp1xpixel+3,spp2ypixel+3, width=1, fill='green')
+        res1ypixel = int(550 - res1ys[i]/4)
+        c.create_line(spp1xpixel, res1ypixel, int(50 + space * spp1xs[i-1]), int(550 - res1ys[i-1]/4), fill = 'blue', width = 2)
+        c.create_rectangle(spp1xpixel-3,res1ypixel-3,spp1xpixel+3,res1ypixel+3, width=1, fill='blue')
+        res2ypixel = int(550 - res2ys[i]/4)
+        c.create_line(spp1xpixel, res2ypixel, int(50 + space * spp1xs[i-1]), int(550 - res2ys[i-1]/4), fill = 'yellow', width = 2)
+        c.create_rectangle(spp1xpixel-3,res2ypixel-3,spp1xpixel+3,res2ypixel+3, width=1, fill='yellow')
+    #root.mainloop()
 
 def test_rstar(to_plot):
     plot_rstar(to_plot)
